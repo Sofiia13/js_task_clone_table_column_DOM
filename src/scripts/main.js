a4b01901aa2e564caebb6770e45b5c1235028c6a
Sofiia13 <sophiyastanish@gmail.com>
2025-06-09 22:40:49 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const columnIndex = 1;
+
+const columnValues = Array.from(table.rows).map(row => {
+  return row.cells[columnIndex].textContent
+});
+
+Array.from(table.rows).forEach((row, i) => {
+  const isHeaderOrFooter = row.parentElement.tagName === 'THEAD' || row.parentElement.tagName === 'TFOOT';
+
+  const cell = isHeaderOrFooter ? document.createElement('th') : document.createElement('td');
+  cell.textContent = columnValues[i];
+
+  const insertIndex = Math.max(0, row.cells.length - 1);
+
+  row.insertBefore(cell, row.cells[insertIndex]);
+});
